88591f2e7bb69955024bc7571f18919afc3a541d
Victor <camposvictor035@gmail.com>
2024-10-24 15:14:32 -0300

    adrien

--- modular-arithmetic/adrien/source.py
+++ modular-arithmetic/adrien/source.py
@@ -8,17 +8,16 @@ FLAG = b'crypto{????????????????????}'
 
 def encrypt_flag(flag):
     ciphertext = []
-    plaintext = ''.join([bin(i)[2:].zfill(8) for i in flag]) # byte to 8-bit binary value
+    plaintext = ''.join([bin(i)[2:].zfill(8) for i in flag]) # byte to 8-bit value
     for b in plaintext:
-        e = randint(1, p) # e is random value between 1 and p
-        n = pow(a, e, p) # calculate modulus of a**e mod p 
+        e = randint(1, p) # e is random value between 1 and p (inclusive)
+        n = pow(a, e, p) # calculate a**e mod p 
         if b == '1':
             ciphertext.append(n)
         else:
-            n = -n % p
+            n = -n % p # additive inverse
             ciphertext.append(n)
     return ciphertext
 
 cipher = encrypt_flag(FLAG)
 print(cipher)
-# each item in list was generated from a bit, n if 1, additive inverse of n if 0
